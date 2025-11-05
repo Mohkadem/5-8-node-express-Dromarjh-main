ce392052feced141ffe786a28c538971728f1749
Mohkadem <s202245820@kfupm.edu.sa>
2025-11-05 09:11:00 +0300

    All done, I didn't push for every task because there was a problem in the pushing (the link for this exersice is provided by doctor hassan so we didn't git it as every time from github

--- 5-7-node-express/backend/server.js
+++ 5-7-node-express/backend/server.js
@@ -1,12 +1,28 @@
 import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
+const app = express();
+app.use(cors());
 
-{/*write code for cors*/}
+{
+  /*write code for cors*/
+}
 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
-{/*write code to define routes*/}
+{
+  app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+  });
+}
 
-
-{/*write code to create server*/}
-
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
+{
+  /*write code to create server*/
+}
